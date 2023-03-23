3930a28c8b8c3da9054e7015cc400e16051ab660
Shu Chen <shuch3n90@gmail.com>
2023-03-23 13:12:09 +0100

    Remove all files in main

--- src/Resources/Constants/Endpoints.ts
+++
@@ -1,4 +0,0 @@
-class Endpoints {
-    public static readonly LIVE: string = 'https://checkout.buckaroo.nl/';
-    public static readonly TEST: string = 'https://testcheckout.buckaroo.nl/';
-}
--- src/Resources/Constants/Gender.ts
+++
@@ -1,6 +0,0 @@
-class Gender {
-    public static readonly UNKNOWN: number = 0;
-    public static readonly MALE: number = 1;
-    public static readonly FEMALE: number = 2;
-    public static readonly NOT_APPLICABLE: number = 9;
-}
