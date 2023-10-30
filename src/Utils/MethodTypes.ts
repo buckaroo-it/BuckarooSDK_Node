1f20a37d468dbc38716f2c6b30b3ebea56e9494d
Vildan Bina <vildanbina@gmail.com>
2023-10-30 16:05:19 +0100

    rename some ts properties

--- src/Utils/MethodTypes.ts
+++ src/Utils/MethodTypes.ts
@@ -1,22 +1,22 @@
 import * as AllRegisteredPaymentMethods from '../PaymentMethods/PaymentMethods';
 
-type InstanceWithManualFlag<T, B extends boolean> = B extends true ? T & { _isManually: B } : T;
+type InstanceWithManualFlag<Method, Manually extends boolean> = Manually extends true ? Method & { _isManually: Manually } : Method;
 
 export type PaymentMethodTypeDictionary = typeof AllRegisteredPaymentMethods;
 export type ServiceCode = keyof PaymentMethodTypeDictionary;
 
-export type PaymentMethodInstanceType<Code extends ServiceCode, B extends boolean = false> = InstanceWithManualFlag<
+export type PaymentMethodInstanceType<Code extends ServiceCode, Manually extends boolean = false> = InstanceWithManualFlag<
     InstanceType<PaymentMethodTypeDictionary[Code]>,
-    B
+    Manually
 >;
 
-export type PaymentMethodRegistryType<K extends ServiceCode, B extends boolean> = {
-    [K in keyof PaymentMethodTypeDictionary]: PaymentMethodInstanceType<K, B>;
+export type PaymentMethodRegistryType<K extends ServiceCode, Manually extends boolean> = {
+    [K in keyof PaymentMethodTypeDictionary]: PaymentMethodInstanceType<K, Manually>;
 }[K];
 
-export function getMethod<Code extends ServiceCode, B extends boolean>(code: Code): PaymentMethodInstanceType<Code, B> {
+export function getMethod<Code extends ServiceCode, Manually extends boolean>(code: Code): PaymentMethodInstanceType<Code, Manually> {
     const methodClass = AllRegisteredPaymentMethods[code] as {
-        new (code: Code): PaymentMethodInstanceType<Code, B>;
+        new (code: Code): PaymentMethodInstanceType<Code, Manually>;
     };
     if (!methodClass) {
         throw new Error(`Invalid payment method code: ${code}`);
