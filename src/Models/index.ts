9af132f7205d6c9c18d6ca803b4bb6742434b2a8
Vildan Bina <vildanbina@gmail.com>
2023-11-02 14:37:54 +0100

    Update index.ts

--- src/Models/index.ts
+++ src/Models/index.ts
@@ -1,10 +1,10 @@
 import IRequest, { IPaymentRequest, IRefundRequest } from './IRequest';
 
+export * from './Model';
 export * from './Interfaces';
 export * from './Response';
 export * from './IParameters';
 export * from './IServiceList';
-export * from './Model';
 export * from './ServiceParameters';
 
 export { IRequest, IPaymentRequest, IRefundRequest };
