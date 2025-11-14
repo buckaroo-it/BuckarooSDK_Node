4a5567029799c2685dbaa45cdec52295431af65b
vildanbina <v.bina@buckaroo-kos.com>
2025-11-14 10:31:34 +0100

    feat(BA-954): add Google Pay integration

---
+++ example/transaction/googlePay.ts
@@ -0,0 +1,21 @@
+import buckarooClient from '../buckarooClient';
+
+const googlepay = buckarooClient.method('googlepay');
+
+//Pay
+googlepay
+    .pay({
+        amountDebit: 10.1,
+        description: 'GooglePay Payment',
+        paymentData: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+        customerCardName: 'XXXXXXX',
+    })
+    .request();
+//Refund
+googlepay
+    .refund({
+        originalTransactionKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+        amountCredit: 10.1,
+        invoice: 'GooglePay Refund',
+    })
+    .request();
